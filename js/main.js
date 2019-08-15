48d24600b024edba6ea7b923e73a7bc5eda4ee2c
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-08-15 11:42:49 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var level = 1;
 var click = {multi: 1.07,rank: 1, bCost: 10,bDamage:10} 
 var baseCostMulti = 1.07; 
 var baseHealthMulti = 1.85; 
-var startingBalls = 18; 
+var startingBalls = 20; 
 var balls = [ 
   {active:false,x:200,y:200,dx:.707,dy:.707,color:"#5555ff",radius:20,bDamage:10,rank:0,cost:10,id:0,clone:false,speed:5}, 
   {active:false,x:300,y:300,dx:.707,dy:-.707,color:"#00ff00",radius:20,bDamage:300,rank:0,cost:300,id:1,clone:false,speed:5}, 
@@ -25,7 +25,9 @@ var balls = [
   {active:false,x:900,y:200,dx:-.707,dy:.707,color:"#008080",radius:35,bDamage:478000000,rank:0,cost:3910000000000,id:14,clone:false,speed:8},
   {active:false,x:400,y:200,dx:-.707,dy:.707,color:"#DC143C",radius:35,bDamage:1430000000,rank:0,cost:23500000000000,id:15,clone:false,speed:8},
   {active:false,x:600,y:100,dx:-.707,dy:.707,color:"#FFD700",radius:35,bDamage:4300000000,rank:0,cost:141000000000000,id:16,clone:false,speed:8},
-  {active:false,x:1000,y:500,dx:-.707,dy:.707,color:"#EE82EE",radius:35,bDamage:12900000000,rank:0,cost:846000000000000,id:17,clone:false,speed:8} 
+  {active:false,x:1000,y:500,dx:-.707,dy:.707,color:"#EE82EE",radius:35,bDamage:12900000000,rank:0,cost:846000000000000,id:17,clone:false,speed:8},
+  {active:false,x:900,y:400,dx:-.707,dy:.707,color:"#A52A2A",radius:40,bDamage:38700000000,rank:0,cost:5070000000000000,id:18,clone:false,speed:9},
+  {active:false,x:500,y:300,dx:-.707,dy:.707,color:"#4B0082",radius:40,bDamage:116000000000,rank:0,cost:30400000000000000,id:19,clone:false,speed:9} 
 ]; 
 var clickBalls = [ 
   {active:false,x:300,y:200,dx:-.707,dy:.707,color:"#666666",radius:25,bDamage:10,rank:1,cost:10,id:100,clone:false,speed:4}, 
