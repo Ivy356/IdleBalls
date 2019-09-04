b54c15398e28c7af619bf83161602129098db0fc
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-09-04 10:00:54 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var level = 1;
 var click = {multi: 1.16,rank: 1, bCost: 10,bDamage:10} 
 var baseCostMulti = 1.10; 
 var baseHealthMulti = 1.85; 
-var startingBalls = 24; 
+var startingBalls = 25; 
 var balls = [ 
   {active:false,x:200,y:200,dx:.707,dy:.707,color:"#5555ff",radius:20,bDamage:10,rank:0,cost:10,id:0,clone:false,speed:5}, 
   {active:false,x:300,y:300,dx:.707,dy:-.707,color:"#00ff00",radius:20,bDamage:300,rank:0,cost:300,id:1,clone:false,speed:5}, 
